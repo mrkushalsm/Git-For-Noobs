c2047327b402320e111e494add174cc0e1c60089
Kushal SM <mrkushalsm@gmail.com>
2024-09-12 15:59:14 +0530

    New project made!
    Changes made by Manas!

--- git-sim.c
+++ git-sim.c
@@ -4,12 +4,30 @@
 #include <process.h>
 
 void init() {
-    // char *working_dir = (char*)malloc(sizeof(char) * 1000);
-    // *working_dir = system("cd");
     printf("Initialized empty Git repository in ");
     system("cd");
     printf("/.git/\n");
 }
+
+struct user{
+    char *name;
+    char *email;
+};
+
+struct user user;
+/**
+void config(){
+    user.name = (char *)malloc(sizeof(char)*256);
+
+
+}
+*/
+void print_error(char *error_message){
+    fprintf(stderr,"[Error]: in %s\n",error_message);
+    return 1;
+}
+
+
 
 int main(int argc, char * argv[]) {
     for(int i = 1; i <= argc - 1; i++) {
